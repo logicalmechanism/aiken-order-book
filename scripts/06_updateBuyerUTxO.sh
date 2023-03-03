bb90a655bd413b678bff8e0aee193574d392b58e
logicalmechanism <m.quinn.p@protonmail.com>
2023-03-03 12:09:12 -0800

    seller and buyer remove and update working

--- scripts/06_updateBuyerUTxO.sh
+++ scripts/06_updateBuyerUTxO.sh
@@ -7,24 +7,26 @@ source .env
 script_path="../order_book.plutus"
 script_address=$(${cli} address build --payment-script-file ${script_path} ${network})
 
-# seller
-seller_address=$(cat wallets/seller-wallet/payment.addr)
-seller_pkh=$(${cli} address key-hash --payment-verification-key-file wallets/seller-wallet/payment.vkey)
+# buyer
+buyer_address=$(cat wallets/buyer-wallet/payment.addr)
+buyer_pkh=$(${cli} address key-hash --payment-verification-key-file wallets/buyer-wallet/payment.vkey)
 
 # collateral
 collat_address=$(cat wallets/collat-wallet/payment.addr)
 collat_pkh=$(${cli} address key-hash --payment-verification-key-file wallets/collat-wallet/payment.vkey)
 
-# asset to trade
-asset="123456789 c34332d539bb554707a2d8826f2057bc628ac433a779c2f43d4a5b5c.5468697349734f6e6553746172746572546f6b656e466f7254657374696e6731"
+#
+asset="12345 6effa18e41008cd0b13f3959a5a4af40b92ca936bb7669f40d3b1f81.5468697349734f6e6553746172746572546f6b656e466f7254657374696e6732"
 
 utxo_value=$(${cli} transaction calculate-min-required-utxo \
     --babbage-era \
     --protocol-params-file tmp/protocol.json \
-    --tx-out-inline-datum-file data/datum/seller_datum.json \
+    --tx-out-inline-datum-file data/datum/buyer_datum.json \
     --tx-out="${script_address} + 5000000 + ${asset}" | tr -dc '0-9')
 
-script_address_out="${script_address} + ${utxo_value} + ${asset}"
+ada_value=$((${utxo_value} + 5000000000))
+ada_value=$((500000 + 100000000))
+script_address_out="${script_address} + ${ada_value}"
 echo "Update OUTPUT: "${script_address_out}
 #
 # exit
@@ -32,16 +34,16 @@ echo "Update OUTPUT: "${script_address_out}
 echo -e "\033[0;36m Gathering Seller UTxO Information  \033[0m"
 ${cli} query utxo \
     ${network} \
-    --address ${seller_address} \
-    --out-file tmp/seller_utxo.json
-TXNS=$(jq length tmp/seller_utxo.json)
+    --address ${buyer_address} \
+    --out-file tmp/buyer_utxo.json
+TXNS=$(jq length tmp/buyer_utxo.json)
 if [ "${TXNS}" -eq "0" ]; then
-   echo -e "\n \033[0;31m NO UTxOs Found At ${seller_address} \033[0m \n";
+   echo -e "\n \033[0;31m NO UTxOs Found At ${buyer_address} \033[0m \n";
    exit;
 fi
 alltxin=""
-TXIN=$(jq -r --arg alltxin "" 'keys[] | . + $alltxin + " --tx-in"' tmp/seller_utxo.json)
-seller_tx_in=${TXIN::-8}
+TXIN=$(jq -r --arg alltxin "" 'keys[] | . + $alltxin + " --tx-in"' tmp/buyer_utxo.json)
+buyer_tx_in=${TXIN::-8}
 
 echo -e "\033[0;36m Gathering Collateral UTxO Information  \033[0m"
 ${cli} query utxo \
@@ -60,7 +62,6 @@ ${cli} query utxo \
     --address ${script_address} \
     ${network} \
     --out-file tmp/script_utxo.json
-
 # transaction variables
 TXNS=$(jq length tmp/script_utxo.json)
 if [ "${TXNS}" -eq "0" ]; then
@@ -68,7 +69,7 @@ if [ "${TXNS}" -eq "0" ]; then
    exit;
 fi
 alltxin=""
-TXIN=$(jq -r --arg alltxin "" --arg sellerPkh "${seller_pkh}" 'to_entries[] | select(.value.inlineDatum.fields[0].fields[0].bytes == $sellerPkh) | .key | . + $alltxin + " --tx-in"' tmp/script_utxo.json)
+TXIN=$(jq -r --arg alltxin "" --arg buyerPkh "${buyer_pkh}" 'to_entries[] | select(.value.inlineDatum.fields[0].fields[0].bytes == $buyerPkh) | .key | . + $alltxin + " --tx-in"' tmp/script_utxo.json)
 script_tx_in=${TXIN::-8}
 
 script_ref_utxo=$(${cli} transaction txid --tx-file tmp/tx-reference-utxo.signed)
@@ -79,17 +80,17 @@ FEE=$(${cli} transaction build \
     --babbage-era \
     --protocol-params-file tmp/protocol.json \
     --out-file tmp/tx.draft \
-    --change-address ${seller_address} \
+    --change-address ${buyer_address} \
     --tx-in-collateral="${collat_utxo}" \
-    --tx-in ${seller_tx_in} \
+    --tx-in ${buyer_tx_in} \
     --tx-in ${script_tx_in} \
     --spending-tx-in-reference="${script_ref_utxo}#1" \
     --spending-plutus-script-v2 \
     --spending-reference-tx-in-inline-datum-present \
     --spending-reference-tx-in-redeemer-file data/redeemer/update_redeemer.json \
     --tx-out="${script_address_out}" \
-    --tx-out-inline-datum-file data/datum/seller_datum.json  \
-    --required-signer-hash ${seller_pkh} \
+    --tx-out-inline-datum-file data/datum/buyer_datum.json \
+    --required-signer-hash ${buyer_pkh} \
     --required-signer-hash ${collat_pkh} \
     ${network})
 
@@ -102,7 +103,7 @@ echo -e "\033[1;32m Fee: \033[0m" $FEE
 #
 echo -e "\033[0;36m Signing \033[0m"
 ${cli} transaction sign \
-    --signing-key-file wallets/seller-wallet/payment.skey \
+    --signing-key-file wallets/buyer-wallet/payment.skey \
     --signing-key-file wallets/collat-wallet/payment.skey \
     --tx-body-file tmp/tx.draft \
     --out-file tmp/tx.signed \
